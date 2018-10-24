4fca3004e866a77f6769409ecc541d9451a378b5
Jerrick Warren <jerrickwarren@MacBook-Pro-3.local>
2018-10-24 15:30:36 -0700

    initial commit

---
+++ NewsFeed/NewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsFeed
+//
+//  Created by Jerrick Warren on 10/24/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
