1ebf39d4f3a4850ce0507a82c8b6980164c85dcc
yzhukovs <yzhukovs@bumnetworks.com>
2018-10-25 21:27:48 -0400

    finished UI and Detail View Controller

--- News/News/TableViewController.swift
+++ News/News/TableViewController.swift
@@ -35,8 +35,15 @@ class TableViewController: UITableViewController {
     }
  
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.performSegue(withIdentifier: "toPVC", sender: tableView)
+        self.performSegue(withIdentifier: "toDetail", sender: tableView)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destination = segue.destination as? DetailsViewController else {return}
+        guard let indexPath = tableView.indexPathForSelectedRow else {return}
+        destination.info = Model.shared.records[indexPath.row]
+    }
+   
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -46,6 +53,7 @@ class TableViewController: UITableViewController {
         cell.titleLabel.text = record.title
         cell.descriptionLabel.text = record.description
         cell.textView.text = record.content
+        cell.layer.borderWidth = 1
         
         ImageLoader.fetchImage(from: record.urlToImage){ image in
             
